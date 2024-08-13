ee961c5de7762f7f0980146fe6ac9a5536b09827
Yurii <wervmer>
2024-08-13 18:45:46 +0300

    Replace the existing BookRepository with JpaRepository
    - deleted BookRepositoryImpl
    - BookRepository now extends JpaRepository

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,10 +1,12 @@
 package com.bookstore.repository;
 
 import com.bookstore.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
 import java.util.Optional;
 
-public interface BookRepository {
+public interface BookRepository extends JpaRepository<Book, Long> {
     Book save(Book book);
 
     List<Book> findAll();
