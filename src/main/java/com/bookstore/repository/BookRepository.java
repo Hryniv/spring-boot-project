90e78faaa8ecb8ce07ad7bfee1a4be4fbc245e0f
Yurii <wervmer>
2024-08-14 14:58:30 +0300

    Add methods for delete and update operations

--- src/main/java/com/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/repository/BookRepository.java
@@ -1,10 +1,9 @@
 package com.bookstore.repository;
 
 import com.bookstore.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Book save(Book book);
